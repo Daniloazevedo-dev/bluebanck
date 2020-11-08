8ae285247323880ec06b12d0f9c9e3fbc4091e03
Danilo <Danilo.ann@gmail.com>
2020-11-08 14:40:01 -0300

    Primeiro commit - Configurações iniciais.

---
+++ src/main/java/br/com/softblue/bluebank/BluebankBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.softblue.bluebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluebankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluebankBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/softblue/bluebank/BluebankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softblue.bluebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluebankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

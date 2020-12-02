cb57cdf1b8f5a3d704ff92749e7dc3ca2157a596
Danilo <Danilo.ann@gmail.com>
2020-12-01 21:21:51 -0300

    Inicio do metodo público de depósito

--- src/main/java/br/com/softblue/bluebank/infrastructure/web/controller/PublicController.java
+++ src/main/java/br/com/softblue/bluebank/infrastructure/web/controller/PublicController.java
@@ -1,12 +1,18 @@
 package br.com.softblue.bluebank.infrastructure.web.controller;
 
+import java.math.BigDecimal;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.softblue.bluebank.application.service.ContaBancariaService;
@@ -15,6 +21,7 @@ import br.com.softblue.bluebank.domain.conta.ContaBancaria;
 import br.com.softblue.bluebank.domain.usuario.Usuario;
 
 @RestController
+@RequestMapping("/public")
 public class PublicController {
     
     @Autowired
@@ -36,6 +43,16 @@ public class PublicController {
 
     }
     
-    
+    @PutMapping(value = "/deposito/{id}", produces = "application/json")
+    public ResponseEntity<String> deposito (
+	    @PathVariable(value = "id") Long id, HttpServletRequest request
+	    ) {
+	
+	String numeroDaConta = request.getParameter("numeroDaConta");
+	String tipoDaConta = request.getParameter("tipoDaConta");
+	BigDecimal deposito = new BigDecimal(request.getParameter("deposito"));
+	
+	return new ResponseEntity<>("Depósito Realizado com sucesso!", HttpStatus.OK);
+    }
 
 }

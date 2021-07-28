7f8ef9fcf83bd28c8f6624271f41658d18ce4002
Danilo <Danilo.ann@gmail.com>
2021-07-28 20:22:38 -0300

    Adicionado dados do usuário no banco

--- src/main/resources/static/_js/home.js
+++ src/main/resources/static/_js/home.js
@@ -1,13 +1,41 @@
-function teste() {
+function setValoresHome() {
 	
-	/*var credenciais = JSON.parse(localStorage.getItem("credenciais"));
+	var credenciais = JSON.parse(localStorage.getItem("credenciais"));
 	
 	if(credenciais !== null ) {
-		console.log(credenciais)
 		displayName.innerHTML = credenciais.displayName;
 		titular.innerHTML = credenciais.displayName;
 		email.innerHTML = credenciais.email;
-	}*/
+		cpf.innerHTML = credenciais.cpf;
+		
+		
+		for(var cont = 0;credenciais.contasBancarias.length; cont++) {
+			
+			let tipo = credenciais.contasBancarias[cont].tipo;
+			
+			if( tipo === "poupanca") {
+				
+				poupanca.innerHTML = credenciais.contasBancarias[cont].numero;
+				
+				if(credenciais.contasBancarias[cont].ativo === true) {
+					contaPoupanca.setAttribute("checked", "true");
+					saldo.innerHTML = credenciais.contasBancarias[cont].saldo.toLocaleString('pt-br', {minimumFractionDigits: 2})
+					
+				}
+				
+			} else if(tipo === "corrente") {
+								
+				corrente.innerHTML = credenciais.contasBancarias[cont].numero;
+				
+				if(credenciais.contasBancarias[cont].ativo === true) {
+					contaCorrente.setAttribute("checked", "true");
+					saldo.innerHTML = credenciais.contasBancarias[cont].saldo.toLocaleString('pt-br', {minimumFractionDigits: 2});
+				}
+				
+			}
+		}
+		
+	}
 }
 
-window.onload = teste
+window.onload = setValoresHome

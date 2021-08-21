542bb0e4ce29cfe2df8155edf0e5f1cedcc0aacc
Danilo <Danilo.ann@gmail.com>
2021-08-21 14:12:23 -0300

    Correção saudação.

--- src/main/resources/static/_js/home.js
+++ src/main/resources/static/_js/home.js
@@ -83,11 +83,13 @@ function alterarCadastro() {
 function setSaudacao() {
 	let dataAtual = new Date();
 	
+	console.log(dataAtual.getHours())
+	
 	if(dataAtual.getHours() >= 1 && dataAtual.getHours() <= 11 ) {
 		saudacao.innerHTML = "Bom dia!";
-	} else if(dataAtual.getHours() >= 12 && dataAtual.getHours() >= 18 ) {
+	} else if(dataAtual.getHours() >= 12 && dataAtual.getHours() <= 18 ) {
 		saudacao.innerHTML = "Boa tarde!";
-	} else if(dataAtual.getHours() > 18 && dataAtual.getHours() <= 23 ) {
+	} else if(dataAtual.getHours() >= 19 && dataAtual.getHours() <= 23 ) {
 		saudacao.innerHTML = "Boa noite!";
 	} else if(dataAtual.getHours() === 0) {
 		saudacao.innerHTML = "Boa noite!";
